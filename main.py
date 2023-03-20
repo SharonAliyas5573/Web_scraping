2661bc89b00d9f3bd121f70f6e844fa1e8da13f3
SharonAliyas5573 <sharonaliyaas25@gmail.com>
2023-03-20 20:12:47 +0530

    fixed some errors

--- main.py
+++ main.py
@@ -2,34 +2,29 @@ from newspaper import Config, Article, fulltext
 from newspaper import build
 import mysql.connector
 
-
 config = Config()
 config.request_timeout = 10
 config.memoize_articles = False 
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="sharon",
-  password="Sharon@2004",
+  user="username",
+  password="",
   database="mydatabase"
 )
 
-
 url = 'https://www.asianetnews.com/latest-news'
-
-
 
 paper = build(url, config=config)
 
 
-article_urls=[]
+
 for article in paper.articles:
     try:
         article.download()
         article.parse()
         article.nlp()
-        article_urls.append(article.url)
-        
+       
 
         url = article.url
         heading = article.title
@@ -37,16 +32,16 @@ for article in paper.articles:
         insert_stmt = "INSERT INTO my_table (url, heading) VALUES (%s, %s)"
         mycursor = mydb.cursor()
         data = (url, heading)
-        print("Insert statement:", insert_stmt)
+        
        
+        mycursor.execute(insert_stmt, data)
         mydb.commit()
-
-
 
         print(article.title)
         
     except:
         print("An error occurred ")
+        continue
 
 mycursor.close()
-mydb.close()
+mydb.close() 
